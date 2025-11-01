6b938e71b59a13e0af2907a074d1f581041f3631
Kush Agarwal <kush.301104@gmail.com>
2025-11-02 00:07:32 +0530

    New Project - Intial Commit

---
+++ src/main/java/in/co/lazylan/bootblog/SpringBootBlogAppApplication.java
@@ -0,0 +1,13 @@
+package in.co.lazylan.bootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlogAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/co/lazylan/bootblog/SpringBootBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.lazylan.bootblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
