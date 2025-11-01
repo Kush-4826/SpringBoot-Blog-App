eecbd7c90aed7b62d1a9142c4d34658dad05ece9
Kush Agarwal <kush.301104@gmail.com>
2025-11-02 00:17:21 +0530

    Created a User Model

---
+++ src/main/java/in/co/lazylan/bootblog/model/User.java
@@ -0,0 +1,25 @@
+package in.co.lazylan.bootblog.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+    private String gender;
+}
