d2cce1ae0480b84df789fde4b7b9da036cef2c60
Kush Agarwal <kush.301104@gmail.com>
2025-11-02 21:21:52 +0530

    Custom Validation Error Response

---
+++ src/main/java/in/co/lazylan/bootblog/response/ValidationErrorResponse.java
@@ -0,0 +1,12 @@
+package in.co.lazylan.bootblog.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class ValidationErrorResponse {
+    private Map<String, String> errors;
+}
