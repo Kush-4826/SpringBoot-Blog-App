ae8296f0129a8d42afd93fb192a2a3e6b212959c
Kush Agarwal <kush.301104@gmail.com>
2025-11-02 09:01:11 +0530

    New Exception for Unfound Resource

---
+++ src/main/java/in/co/lazylan/bootblog/exception/ResourceNotFoundException.java
@@ -0,0 +1,19 @@
+package in.co.lazylan.bootblog.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ResourceNotFoundException extends Exception {
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s: %s", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
