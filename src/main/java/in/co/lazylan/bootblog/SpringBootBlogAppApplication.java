730c0c7cc5bedb5b14ea8090a5e177b6d799c1ec
Kush Agarwal <kush.301104@gmail.com>
2025-11-02 08:47:14 +0530

    Creating a ModelMapper Bean

--- src/main/java/in/co/lazylan/bootblog/SpringBootBlogAppApplication.java
+++ src/main/java/in/co/lazylan/bootblog/SpringBootBlogAppApplication.java
@@ -1,7 +1,9 @@
 package in.co.lazylan.bootblog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootBlogAppApplication {
@@ -10,4 +12,14 @@ public class SpringBootBlogAppApplication {
         SpringApplication.run(SpringBootBlogAppApplication.class, args);
     }
 
+    /**
+     * Creating a ModelMapper Bean in the IoC Container
+     * so that I can access it to map my models to their corresponding DTOs.
+     *
+     * @return
+     */
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
