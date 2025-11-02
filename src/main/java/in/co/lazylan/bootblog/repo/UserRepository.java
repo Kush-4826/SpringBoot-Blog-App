feb82467a9958b021ab9f524e0791905b885b3af
Kush Agarwal <kush.301104@gmail.com>
2025-11-02 09:01:31 +0530

    Added Method to find User by Email

--- src/main/java/in/co/lazylan/bootblog/repo/UserRepository.java
+++ src/main/java/in/co/lazylan/bootblog/repo/UserRepository.java
@@ -3,5 +3,8 @@ package in.co.lazylan.bootblog.repo;
 import in.co.lazylan.bootblog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
