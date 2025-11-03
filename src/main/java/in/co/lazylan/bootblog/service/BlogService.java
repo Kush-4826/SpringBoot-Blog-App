2c48033c0a3d36aab5d4de13072f30669a9a2e4b
Kush Agarwal <kush.301104@gmail.com>
2025-11-03 13:51:41 +0530

    Created a Blog Service

---
+++ src/main/java/in/co/lazylan/bootblog/service/BlogService.java
@@ -0,0 +1,26 @@
+package in.co.lazylan.bootblog.service;
+
+import in.co.lazylan.bootblog.exception.ResourceNotFoundException;
+import in.co.lazylan.bootblog.payload.BlogDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface BlogService {
+    BlogDto createBlog(BlogDto blogDto, String authorId, String categoryId) throws ResourceNotFoundException;
+
+    BlogDto updateBlog(BlogDto blogDto, String id);
+
+    void deleteBlogById(String id);
+
+    BlogDto getBlogById(String id);
+
+    List<BlogDto> getAllBlogs();
+
+    List<BlogDto> getBlogByCategory(String id);
+
+    List<BlogDto> getBlogByAuthor(String id);
+
+    List<BlogDto> searchBlog(String keyword);
+}
