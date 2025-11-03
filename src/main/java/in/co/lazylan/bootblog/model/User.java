90e33c72837b77fc13e9b74e5cd8e5768927a31a
Kush Agarwal <kush.301104@gmail.com>
2025-11-03 09:03:01 +0530

    Added Necessary Database Level Constraints

--- src/main/java/in/co/lazylan/bootblog/model/User.java
+++ src/main/java/in/co/lazylan/bootblog/model/User.java
@@ -15,9 +15,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    @Column(unique = true, nullable = false)
     private String username;
+    @Column(nullable = false)
     private String password;
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String gender;
     @Lob
     private String about;
