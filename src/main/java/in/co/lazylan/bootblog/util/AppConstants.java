c2321da8e5d7ba2e0bb1408fd4f42a73d3f00fba
Kush Agarwal <kush.301104@gmail.com>
2025-11-04 19:56:50 +0530

    Created Constants File

---
+++ src/main/java/in/co/lazylan/bootblog/util/AppConstants.java
@@ -0,0 +1,10 @@
+package in.co.lazylan.bootblog.util;
+
+public interface AppConstants {
+    String PAGE_PARAM_NAME = "page";
+    String SORT_BY_PARAM_NAME = "sortBy";
+    String ORDER_BY_PARAM_NAME = "order";
+    String SEARCH_QUERY_PARAM_NAME = "q";
+    String DEFAULT_PAGE_NUMBER = "0";
+    int DEFAULT_PAGE_SIZE = 3;
+}
