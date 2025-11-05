5058bb567bc1516fa9b7367be57a13483245f332
Kush Agarwal <kush.301104@gmail.com>
2025-11-05 17:30:34 +0530

    Increased Page Size

--- src/main/java/in/co/lazylan/bootblog/util/AppConstants.java
+++ src/main/java/in/co/lazylan/bootblog/util/AppConstants.java
@@ -6,5 +6,5 @@ public interface AppConstants {
     String ORDER_BY_PARAM_NAME = "order";
     String SEARCH_QUERY_PARAM_NAME = "q";
     String DEFAULT_PAGE_NUMBER = "0";
-    int DEFAULT_PAGE_SIZE = 3;
+    int DEFAULT_PAGE_SIZE = 10;
 }
