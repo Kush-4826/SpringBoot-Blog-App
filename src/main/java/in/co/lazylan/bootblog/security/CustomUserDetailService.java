deb16cbde5fa9b3d3ac6870367c6ed6e36c0efec
Kush Agarwal <kush.301104@gmail.com>
2025-11-05 07:21:46 +0530

    Create Custom UserDetailsService to fetch user from DB

---
+++ src/main/java/in/co/lazylan/bootblog/security/CustomUserDetailService.java
@@ -0,0 +1,37 @@
+package in.co.lazylan.bootblog.security;
+
+import in.co.lazylan.bootblog.model.User;
+import in.co.lazylan.bootblog.repo.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * We'll be loading a user from the database in this method.
+     * Whenever we need to get the user details from some custom source (Database in this case),
+     * we need to create our own implementation of the UserDetailsService and then
+     * implement this method. In this method, now we'll fetch the user from the database
+     * based on the username.
+     *
+     * @param username the username identifying the user whose data is required.
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + username + " not found"));
+        return user;
+    }
+}
