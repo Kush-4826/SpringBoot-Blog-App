c34a88e29e6fade1f628a14afbfd946d73766bbf
Kush Agarwal <kush.301104@gmail.com>
2025-11-05 21:05:56 +0530

    Added Repository method to fetch blog comments

--- src/main/java/in/co/lazylan/bootblog/repo/CommentRepository.java
+++ src/main/java/in/co/lazylan/bootblog/repo/CommentRepository.java
@@ -1,7 +1,11 @@
 package in.co.lazylan.bootblog.repo;
 
+import in.co.lazylan.bootblog.model.Blog;
 import in.co.lazylan.bootblog.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    Page<Comment> findByBlog(Blog blog, Pageable pageable);
 }
