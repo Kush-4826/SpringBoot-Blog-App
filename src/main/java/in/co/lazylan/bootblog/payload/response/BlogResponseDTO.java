31a4cbc48393c5b3abe43d9cc1028923b2c7c369
Kush Agarwal <kush.301104@gmail.com>
2025-11-05 17:30:11 +0530

    Removed Comments from BlogResponse

--- src/main/java/in/co/lazylan/bootblog/payload/response/BlogResponseDTO.java
+++ src/main/java/in/co/lazylan/bootblog/payload/response/BlogResponseDTO.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -28,6 +27,4 @@ public class BlogResponseDTO {
     private CategoryResponseDTO category;
 
     private UserResponseDTO author;
-
-    private List<CommentResponseDTO> comments;
 }
