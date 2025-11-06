098a5201a58ce1dab67218f71c7539a485b56ac9
Kush Agarwal <kush.301104@gmail.com>
2025-11-06 22:06:39 +0530

    Accepted Authenticated User in controller

--- src/main/java/in/co/lazylan/bootblog/controller/CommentController.java
+++ src/main/java/in/co/lazylan/bootblog/controller/CommentController.java
@@ -1,12 +1,15 @@
 package in.co.lazylan.bootblog.controller;
 
 import in.co.lazylan.bootblog.exception.ResourceNotFoundException;
+import in.co.lazylan.bootblog.model.User;
 import in.co.lazylan.bootblog.payload.request.CommentRequestDTO;
 import in.co.lazylan.bootblog.payload.response.CommentResponseDTO;
 import in.co.lazylan.bootblog.response.SuccessResponse;
 import in.co.lazylan.bootblog.service.CommentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,17 +24,19 @@ public class CommentController extends ApiController {
 
     @PostMapping("")
     public ResponseEntity<CommentResponseDTO> store(
-            @RequestBody CommentRequestDTO comment
+            @RequestBody CommentRequestDTO comment,
+            @AuthenticationPrincipal User user
     ) throws ResourceNotFoundException {
-        CommentResponseDTO commentResponseDTO = this.commentService.createComment(comment);
+        CommentResponseDTO commentResponseDTO = this.commentService.createComment(comment, user);
         return new ResponseEntity<>(commentResponseDTO, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{commentId}")
     public ResponseEntity<SuccessResponse> delete(
-            @PathVariable int commentId
-    ) throws ResourceNotFoundException {
-        this.commentService.deleteComment(commentId);
+            @PathVariable int commentId,
+            @AuthenticationPrincipal User user
+    ) throws ResourceNotFoundException, AccessDeniedException {
+        this.commentService.deleteComment(commentId, user);
         return new ResponseEntity<>(new SuccessResponse("Comment Deleted Successfully"), HttpStatus.OK);
     }
 }
