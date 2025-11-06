738f7a36a20cbefe9bfdc3304fef7bfa7e883417
Kush Agarwal <kush.301104@gmail.com>
2025-11-06 13:55:16 +0530

    Removed Roles Entity

--- src/main/java/in/co/lazylan/bootblog/repo/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package in.co.lazylan.bootblog.repo;
-
-import in.co.lazylan.bootblog.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
