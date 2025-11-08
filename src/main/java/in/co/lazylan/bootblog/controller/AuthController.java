0e84eac5b1fd269e616c4af317d1bee67be54ac9
Kush Agarwal <kush.301104@gmail.com>
2025-11-08 14:14:42 +0530

    Changed CORS

--- src/main/java/in/co/lazylan/bootblog/controller/AuthController.java
+++ src/main/java/in/co/lazylan/bootblog/controller/AuthController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://192.168.1.100:5173")
 public class AuthController extends ApiController {
 
     private final UserDetailsService userDetailsService;
